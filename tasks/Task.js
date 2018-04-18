a1bcc11e7748a8ca19b5d93d74d9e17e1ad1153e
Farhankhan Daya <lobabob@users.noreply.github.com>
2018-04-18 16:01:49 -0700

    Fixed some linting errors.

--- tasks/Task.js
+++ tasks/Task.js
@@ -3,11 +3,11 @@
 /**
  * TODO This should be a base class/function that all other Tasks can extend.
  * Should look something like:
- * 
+ *
  * class Task {
  *   constructor() {}
  *   interval() {
- *     return 1 day;  // Indicates that this task should be run daily. 
+ *     return 1 day;  // Indicates that this task should be run daily.
  *                    // Could also be something like return 12345 so it runs every 12345 seconds
  *   }
  *   action () {
